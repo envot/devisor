b4cf0b05b15dbee652e045b850d377843acc3b33
schueppi <schueppi@schueppi.com>
2020-09-22 22:40:24 +0200

    Added TestDevice.

---
+++ Device-TestDevice/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="device-testdevice",
+    version="0.0.1",
+    author="schueppi",
+    author_email="schueppi@schueppi.com",
+    description="A device for testing devisor's features.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
