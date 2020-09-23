026310d9022004fcbc847e311d925e275bb15250
schueppi <schueppi@schueppi.com>
2020-09-23 23:06:36 +0200

    Added new version number of testdevice

--- Device-TestDevice/setup.py
+++ Device-TestDevice/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="device-testdevice",
-    version="0.0.1",
+    version="0.0.2",
     author="schueppi",
     author_email="schueppi@schueppi.com",
     description="A device for testing devisor's features.",
