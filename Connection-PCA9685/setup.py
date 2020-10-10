f2dfed54fdc68db3bacd41754d3369fd2266a156
schueppi <schueppi@schueppi.com>
2020-10-11 00:21:10 +0200

    Added Connection to PCA9685 chip.

---
+++ Connection-PCA9685/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="connection-pca9685",
+    version="0.0.1",
+    author="schueppi",
+    author_email="schueppi@schueppi.com",
+    description="A class to providing a pca9685 connection over an i2cbus.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
