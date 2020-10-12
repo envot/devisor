69d958cac2547104304bece88ea723f6b4f64e5e
schueppi <schueppi@schueppi.com>
2020-10-12 11:39:38 +0200

    Updated pwm connection..

--- Connection-PWM/pwm/__init__.py
+++ Connection-PWM/pwm/__init__.py
@@ -1,1 +1,19 @@
-from .driver import *
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+
+# Python program to control, monitor and configure devices in a EoT: https://envot.io
+# Klemens Schueppert : schueppi@envot.io
+
+class ConnectionClass():
+    def __init__(self, devisor, address='pca9685,smbus,1,0x40,0'):
+        self.devisor = devisor 
+        addressArray = address.split(',')
+        addressArray[0] = 'pwm'+addressArray[0]
+        self.con = self.devisor.runningConnections.open(
+                ','.join(addressArray))
+
+    def set_pwm(self, value):
+        return self.con.set_pwm(value)
+
+    def set_phase(self, value):
+        return self.con.set_phase(value)
--- Connection-PWM/setup.py
+++ Connection-PWM/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="connection-pwm",
-    version="0.0.1",
+    version="0.0.2",
     author="schueppi",
     author_email="schueppi@schueppi.com",
     description="A class providing a pwm item.",
