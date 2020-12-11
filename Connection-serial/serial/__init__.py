7ad7717cac9d383049557ce9592665ee9ed1c875
schueppi <schueppi@schueppi.com>
2020-12-11 18:19:09 +0100

    New Connection-serial.

---
+++ Connection-serial/serial/__init__.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+
+# Python program to control, monitor and configure devices in a EoT: https://envot.io
+# Klemens Schueppert : schueppi@envot.io
+import serial 
+import sys
+
+class ConnectionClass():
+    def __init__(self, devisor, address='/dev/ttyUSB0'):
+        self.devsor = devisor 
+        self.address = address
+        self.addressArray = address.split(',')
+        self.EOL = '\r\n'
+        self.codec = 'ASCII'
+        self.maxTries = 10
+        self.open()
+
+    def write(self, value):
+        return self.instr.write((value+self.EOL).encode(self.codec))
+
+    def ask(self, value):
+        self.write(value)
+        data = ''
+        try:
+            tries = 0
+            while not data.endswith(self.EOL) and tries<self.maxTries:
+                tries += 1
+                data += self.instr.read(1024).decode(self.codec)
+            self.failure = 0
+            return data[:-len(self.EOL)]
+        except Exception:
+            err = sys.exc_info()[1]
+            self.failure += 1
+            if self.failure < 2:
+                self.devisor.log.new_log('Connection "socket" failed to ask for "'
+                    +value+'":'+str(err), "WARNING")
+                data = self.ask(value)
+            elif self.failure < 3:
+                self.devisor.log.new_log('Connection "socket" failed again to ask for "'
+                    +value+'":'+str(err)+"... now reconnect socket.", "WARNING")
+                self.reconnect()
+                data = self.ask(value)
+            else:
+                self.devisor.log.new_log('Connection "socket" failed after reconnect to ask for "'
+                    +value+'":'+str(err)+"... return empty String.", "ERROR")
+            return data 
+
+    def open(self):
+        self.instr = serial.Serial(self.addressArray[0])
+        self.instr.timeout = 0.3
+
+    def close(self):
+        self.instr.close()
+        del(self.instr)
+
+    def reonnect(self):
+        self.close()
+        self.open()
---
+++ Connection-serial/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="connection-serial",
+    version="0.1.0",
+    author="schueppi",
+    author_email="schueppi@schueppi.com",
+    description="A class to providing a serial connection through pySerial.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
