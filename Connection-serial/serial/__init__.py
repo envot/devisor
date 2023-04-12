e80fd476125e40386b222330e8b27b0f8b198591
schueppi <schueppi@schueppi.com>
2023-04-12 15:23:20 +0200

    Fixed serial.

--- Connection-serial/serial/__init__.py
+++ Connection-serial/serial/__init__.py
@@ -1,63 +1,54 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 # Python program to control, monitor and configure devices in a EoT: https://envot.io
 # Klemens Schueppert : schueppi@envot.io
-import serial 
+import serial
 import sys
 
 class ConnectionClass():
     def __init__(self, devisor, address='/dev/ttyUSB0'):
-        self.devsor = devisor 
+        self.devisor = devisor
         self.address = address
         self.addressArray = address.split(',')
-        self.EOL = '\r\n'
+        self.eol = '\r\n'
         self.codec = 'ASCII'
-        self.maxTries = 10
         self.open()
 
-    def write(self, value):
-        return self.instr.write((value+self.EOL).encode(self.codec))
+    def write(self, value, codec=True):
+        if not type(codec)==str:
+            if codec:
+                codec = self.codec
+            else:
+                codec = ''
+        return self.instr.write((value+self.eol).encode(self.codec))
 
     def read(self, length=1024, codec=True):
+        dataRaw = self.instr.read(length)
         if type(codec)==str:
-            return self.instr.recv(length).decode(codec)
+            return self.decode(dataRaw, codec)
         elif codec:
-            return self.instr.recv(length).decode(self.codec)
+            return self.decode(dataRaw, self.codec)
         else:
-            return self.instr.recv(length)
+            return dataRaw
 
-
-    def ask(self, value):
-        self.write(value)
-        data = ''
+    def decode(self, dataRaw, codec):
         try:
-            tries = 0
-            while not data.endswith(self.EOL) and tries<self.maxTries:
-                tries += 1
-                data += self.instr.read(1024).decode(self.codec)
-            self.failure = 0
-            return data[:-len(self.EOL)]
+            data = dataRaw.decode(codec)
         except Exception:
             err = sys.exc_info()[1]
-            self.failure += 1
-            if self.failure < 2:
-                self.devisor.log.new_log('Connection "socket" failed to ask for "'
-                    +value+'":'+str(err), "WARNING")
-                data = self.ask(value)
-            elif self.failure < 3:
-                self.devisor.log.new_log('Connection "socket" failed again to ask for "'
-                    +value+'":'+str(err)+"... now reconnect socket.", "WARNING")
-                self.reconnect()
-                data = self.ask(value)
-            else:
-                self.devisor.log.new_log('Connection "socket" failed after reconnect to ask for "'
-                    +value+'":'+str(err)+"... return empty String.", "ERROR")
-            return data 
+            self.devisor.log.new_log('Received data could not be decoeded with codec: '
+                    +str(codec)+' due to: '+str(err), "WARNING")
+            data = dataRaw
+        return data
+
 
     def open(self):
-        self.instr = serial.Serial(self.addressArray[0])
-        self.instr.timeout = 0.3
+        self.instr = serial.Serial(self.addressArray[0],
+                baudrate = 115200, bytesize=8, parity=serial.PARITY_NONE,
+                stopbits=1, xonxoff=0, rtscts=0, timeout=0.1)
+        self.devisor.log.new_log('Connection "serial" successfully connect to "'
+                    +self.addressArray[0]+'"', "INFO")
 
     def close(self):
         self.instr.close()
