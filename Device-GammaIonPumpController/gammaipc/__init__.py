30af4319c80796812f3548a9f9aa9bd8484b802c
Ceki99 <antonio.cerovic@gmail.com>
2024-03-05 08:23:00 +0100

    Fixed device addresses.

--- Device-GammaIonPumpController/gammaipc/__init__.py
+++ Device-GammaIonPumpController/gammaipc/__init__.py
@@ -386,7 +386,7 @@ class DeviceClass(DeviceBase):
     This class is used to communicate with Gamma Ion Vacuum Pump Controller device over the Telnet connection.
     """
 
-    def init_pre(self, address="localhost:7023"): #10.187.144.25
+    def init_pre(self, address="ip-ca.lab:23"):
         """
         Constructor of the class.
 
